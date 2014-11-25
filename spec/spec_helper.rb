d21e37f35cbe52a7e4f80ce03a4d4e504e666d95
Aram Price <aprice@pivotal.io>
2014-11-25 13:03:18 -0800

    spec_helper: remove auto-filter on :focus; stop displaying warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,12 +7,9 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  config.filter_run :focus
   config.run_all_when_everything_filtered = true
 
   config.disable_monkey_patching!
-
-  config.warnings = true
 
   if config.files_to_run.one?
     config.default_formatter = 'doc'
